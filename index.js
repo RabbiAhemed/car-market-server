3f55b2da851a17498f3f14e46837bf10891ce618
Rabbi Ahmed Limon <rabbiahemed>
2022-11-27 01:39:19 +0600

    get all categories

--- index.js
+++ index.js
@@ -9,15 +9,34 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.esmmy.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    const categoriesCollection = client
+      .db("ResaleZone")
+      .collection("categories");
+    // all categories
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const cursor = categoriesCollection.find(query);
+      const categories = await cursor.toArray();
+
+      res.send(categories);
+    });
+  } finally {
+  }
+}
+
+run().catch((error) => console.log(error));
+
 app.get("/", (req, res) => {
-  res.send("server live");
+  res.send("ResaleZone server is live");
 });
 
 app.listen(port, () => {
