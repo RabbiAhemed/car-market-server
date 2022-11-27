71dd7d7f4e4c5ec96d643f64c04fe22de61b67fc
Rabbi Ahmed Limon <rabbiahemed>
2022-11-27 20:56:29 +0600

    jwt generate

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,23 +23,44 @@ async function run() {
       .db("ResaleZone")
       .collection("categories");
     const productsCollection = client.db("ResaleZone").collection("products");
+    const usersCollection = client.db("ResaleZone").collection("users");
+
+    // generate jwt and save users email
+    app.put("/user/:email", async (req, res) => {
+      const email = req?.params.email;
+      const filter = { email: email };
+      const user = req.body;
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      console.log(result);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1d",
+      });
+      console.log(token);
+      res.send({ result, token });
+    });
+
     // get all categories
     app.get("/categories", async (req, res) => {
       const query = {};
       const cursor = categoriesCollection.find(query);
       const categories = await cursor.toArray();
-
       res.send(categories);
     });
     // all products under a category
     app.get("/categories/:category_id", async (req, res) => {
       const id = req?.params?.category_id;
-      console.log(typeof id);
       const query = { category_id: id };
       const cursor = productsCollection.find(query);
       const products = await cursor.toArray();
       res.send(products);
-      console.log(products.length);
     });
     //
   } finally {
